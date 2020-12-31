855b89caadf892a1f01df74dbba4ca5aff5ad6e2
kissmilan91 <kissmilan91@gmail.com>
2020-12-31 17:59:50 +0100

    Legnagyobb_Paratlan

--- luxor.py
+++ luxor.py
@@ -71,7 +71,7 @@ def main() -> None:
     # teljes_szamsor = []
     # teljes_szamsor.append(l_rendezve + u_rendezve + x_rendezve + o_rendezve + r_rendezve)
 
-    print(teljes_szamsor)
+    print(f'A kihúzott számok: {teljes_szamsor}')
 
     with open('szamok.txt', 'a', encoding='utf-8') as szamsor:
         szoveg = 'Luxor sorsolás eredménye: '
@@ -81,6 +81,33 @@ def main() -> None:
         szamsor.write((ido.strftime("%Y %B %d %X ")))
         szamsor.writelines(str(teljes_szamsor))
         szamsor.write(soremeles)
+
+    paros_szamok: List[int] = []
+    paratlan_szamok: List[int] = []
+    szamok = teljes_szamsor
+    for i in szamok:
+        if i % 2 == 0:
+            paros_szamok.append(i)
+    for j in szamok:
+        if j % 2 == 1:
+            paratlan_szamok.append(j)
+
+    print(f'A kihúzott páros számok: {paros_szamok}')
+    print(f'A kihúzott páratlan számok: {paratlan_szamok}')
+
+    #  legnagyobb páratlan szám értékét és indexét a listában
+    legnagyobb_paratlan: int = -1
+    for i, item in enumerate(teljes_szamsor):
+        if item % 2 == 1:   # Ha páratlan
+            if legnagyobb_paratlan == -1:   # Az első páratlan szám a listában
+                legnagyobb_paratlan = i   # Erre tekintünk a legnagyobb páratlan számként
+            else:
+                if item > szamok[legnagyobb_paratlan]:
+                    legnagyobb_paratlan = i
+    if legnagyobb_paratlan == -1:
+        print('Nincs a listában páratlan szám')
+    else:
+        print(f'A legnagyobb páratlan szám értéke: {szamok[legnagyobb_paratlan]}, indexe: {legnagyobb_paratlan}')
 
 
 if __name__ == "__main__":
